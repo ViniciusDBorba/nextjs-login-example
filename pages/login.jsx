65f138978a70b247a83e989fc8826500312f2ad1
Vinicius de Borba <vinicius_deborba@hotmail.com>
2022-02-02 00:44:27 -0300

    create login page

--- pages/login.jsx
+++ pages/login.jsx
@@ -2,8 +2,17 @@ import styles from '../styles/login.module.css'
 
 const Login = () => {
     return (
-        <div className={styles.card}>
-            
+        <div className={styles.body}>
+            <div className={styles.card}>
+                <p className={styles.card_title}>Bem vindo</p>
+
+                <input className={styles.input} type='text' placeholder='Login'/>
+                <input className={styles.input} type='text' placeholder='Senha'/>
+
+                <button className={styles.button}>
+                    <p className={styles.button_text}>Entrar</p>
+                </button>
+            </div>
         </div>
     )
 }
